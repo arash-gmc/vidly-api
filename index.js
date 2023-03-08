db74214de78dfdb6f9b1942d64554d0881990653
arash-gmc <arash.green.423@gmail.com>
2023-03-08 17:36:37 +0330

    root aded

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ require('./startups/routes')(app)
 require('./startups/validations')()
 
 
-
+app.get('/',(req,res)=>{
+	res.send('This is a vidy API')
+})
 
 const port = process.env.PORT || config.get('port') || 3000
 const server = app.listen(port,()=>winston.info(`listening on port ${port}`))
