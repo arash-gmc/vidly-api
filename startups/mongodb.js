678aba07e3ffe36964a8dfafec94c4c09d592bff
arash-gmc <arash.green.423@gmail.com>
2023-03-08 17:45:57 +0330

    db connection options

--- startups/mongodb.js
+++ startups/mongodb.js
@@ -3,7 +3,8 @@ const winston = require('winston');
 const config = require('config')
 
 const db = config.get('db') 
-mongoose.connect(db)
-	.then(()=>winston.info(`connected to ${db}...`))
+mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true})
+	.then(()=>winston.info(`connected to databse...`))
+	.catch(e=>{throw Error('db connection error: ',e)})
 
 
