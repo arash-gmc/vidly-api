6048c9f15e1d73c0c75001e6b8056f6b4192a5f2
arash-gmc <arash.green.423@gmail.com>
2023-03-08 17:08:44 +0330

    db edit

--- startups/mongodb.js
+++ startups/mongodb.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const winston = require('winston');
 const config = require('config')
 
-//const db = config.get('db') 
-const db = 'mongodb+srv://arash77gh:ash2lo2shk@cluster0.itb6nsj.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(db,{useNewUrlParser:true,useUnifiedTopology:true})
+const db = config.get('db') 
+mongoose.connect(db)
 	.then(()=>winston.info(`connected to ${db}...`))
 
 
